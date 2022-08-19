1467daa1b2546b68dad5d4ed77eed77b654c04f7
Jayden Cowley <jcowley16@gmail.com>
2022-08-19 07:42:28 -0600

    linking html and js, setting up querySelectors

---
+++ assets/js/index.js
@@ -0,0 +1,4 @@
+var timer = document.querySelector('#timer');
+var startButton = document.querySelector('#start');
+var qContainer = document.querySelector('#quiz-questions');
+var highScores = document.querySelector('#highscores');
